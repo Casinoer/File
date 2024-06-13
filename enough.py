ea2be29962352ae4b54fe8883650651dacc2e456
Casino.exe <122696498+Casinoer@users.noreply.github.com>
2024-06-13 15:09:55 +0300

    Update enough.py

--- enough.py
+++ enough.py
@@ -27,7 +27,7 @@ while 1:
   `%888"  4888!`     `~    "    `"`     `Y"   'YP       'Y"
     `"      ""
     
-    Sms: {}           {}{}- Umut Kahraman\n  
+    {}{}- Umut Kahraman\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
